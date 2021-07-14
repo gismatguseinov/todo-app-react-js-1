541c3dd4aa8ba4b0fe438dc14e2ea978b20545c9
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-07-14 11:05:30 +0400

    When data Table or any component without data, show empty component

---
+++ src/Empty/Empty.js
@@ -0,0 +1,16 @@
+import { Empty } from "antd";
+import React from "react";
+const EmptyComp = () => {
+  return (
+    <div
+      style={{
+        marginLeft: "47%",
+        marginTop: "18%",
+      }}
+    >
+      <Empty description={false}/>
+    </div>
+  );
+};
+
+export default EmptyComp;
